81cc468acd75e8e6e94478d33fd27c998248e768
Matthew Cassar <matcassar@deloitte.com.mt>
2017-03-12 11:18:55 +0100

    added event files

--- public/src/components/eventresults/eventresults.component.js
+++ public/src/components/eventresults/eventresults.component.js
@@ -5,9 +5,28 @@
     .component('eventresults', {
       templateUrl: 'components/eventresults/template.html',
       controller: function ($scope, $mdDialog, $http) {
-        console.log("TOTO")
-        
+      
+       this.tagList = $http.get("http://localhost:3000/event/byTags/restaurant")
+          .then(function (res) {
+            this.tagList = res.data;
+            console.log(JSON.stringify(this.tagList))
+          }.bind(this))
+          .catch(function (err) {
+            console.error(err);
+          });;
+
+      
+      let pplCount = $http.get("http://localhost:3000/event/interestCount/3")
+          .then(function (ress) {
+            pplCount = ress.data;
+            console.log(JSON.stringify(pplCount))
+          }.bind(this))
+          .catch(function (err) {
+            console.error(err);
+          });;
     }
+
+
 });
 
 })(window.angular);
