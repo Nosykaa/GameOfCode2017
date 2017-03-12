707c9cf0c1bc47b5d78136a61a191699adcb3bb6
Matthew Cassar <matcassar@deloitte.com.mt>
2017-03-12 12:16:59 +0100

    added logging files

--- public/src/components/eventresults/eventresults.component.js
+++ public/src/components/eventresults/eventresults.component.js
@@ -5,32 +5,19 @@
     .component('eventresults', {
       templateUrl: 'components/eventresults/template.html',
 
-      controller: function ($scope, $mdDialog, $http, myService) {
-       $scope.selectedTag = myService.get(function(res){
-          $scope.selectedTag = res;
-        });
-        
-       this.tagList = $http.get("http://localhost:3000/event/byTags/restaurant")
+      controller: function ($scope, $mdDialog, $http,myService) {
+      this.tagList =  myService.get(function(res){
+      this.tagList = $http.get("http://localhost:3000/event/byTags/" + res)
           .then(function (res) {
             this.tagList = res.data;
             console.log(JSON.stringify(this.tagList))
           }.bind(this))
-          .catch(function (err) {
+           .catch(function (err) {
             console.error(err);
           });;
-
-      
-      let pplCount = $http.get("http://localhost:3000/event/interestCount/3")
-          .then(function (ress) {
-            pplCount = ress.data;
-            console.log(JSON.stringify(pplCount))
-          }.bind(this))
-          .catch(function (err) {
-            console.error(err);
-          });;       
+      }.bind(this))
+      console.log(this.tagList)
     }
-
-
 });
 
 })(window.angular);
